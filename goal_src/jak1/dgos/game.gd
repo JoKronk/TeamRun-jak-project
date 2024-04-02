9293097b0ad8085e1114d135945fc8dd6f5a77af
Joakim <48765374+JoKronk@users.noreply.github.com>
2024-04-02 15:11:13 +0300

    Revert "Fix player name draw outside of debug mode"
    This reverts commit 0638cfae57a23157e5f2a3c545bfd6747d7a1bbf.

--- goal_src/jak1/dgos/game.gd
+++ goal_src/jak1/dgos/game.gd
@@ -348,7 +348,6 @@
   "ticky.o"
   "hud-classes-pc.o" ;; added
   "mod-settings.o" ;; added
-  "mod-text-render.o" ;; added
   "mod-common-functions.o" ;; added
   "mod-custom-code.o" ;; added
   ;; keep zoomer stuff loaded
